3d36862cc3e85ab2f9b64f703a82428d17e0bf97
Jérome <jerome.piquet@atinternet.com>
2019-11-16 23:14:06 +0100

    fix no-padding warning

--- projects/ionic4-datepicker/src/lib/ionic4-datepicker-modal/ionic4-datepicker-modal.component.ts
+++ projects/ionic4-datepicker/src/lib/ionic4-datepicker-modal/ionic4-datepicker-modal.component.ts
@@ -12,7 +12,7 @@ const moment = moment_;
 })
 export class Ionic4DatepickerModalComponent implements OnInit, OnDestroy {
 
-  @ViewChild(IonContent) content: IonContent;
+  @ViewChild(IonContent, {static: true}) content: IonContent;
 
   currentDate;
   today;
